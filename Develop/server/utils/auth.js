f1bcedde16f620a63acf40266e1d237e21e682f9
wacwestley30 <westleycervantes@gmail.com>
2024-05-30 20:22:52 -0400

    Initial Commit

---
+++ Develop/server/utils/auth.js
@@ -0,0 +1,43 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  // Added Auth Error from GraphQL
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { authenticatedPerson } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = authenticatedPerson;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ authenticatedPerson: payload }, secret, { expiresIn: expiration });
+  },
+};
---
+++ README.md
@@ -0,0 +1,42 @@
+# React Portfolio Project
+
+[![License](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+n
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+n
+
+## Usage
+
+n
+
+## License
+
+Licensed under the MIT license.
+
+[MIT License](https://opensource.org/licenses/MIT)
+
+## Contributing
+
+
+
+## Tests
+
+n
+
+## Questions
+
+For any questions, please contact WestleyCervantes@gmail.com. Visit [wacwestley30](https://github.com/wacwestley30) for more projects.
