f7f6656d85d029d92973ea3198b08efc22e87e48
Noodle77 <anonymous@example.com>
2024-06-06 21:03:02 -0500

    updated groups field from an embedded subdoc to a reference and added a populateGroups method

--- Develop/server/models/User.js
+++ Develop/server/models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
+    
     firstName: {
       type: String,
       required: false,
@@ -41,7 +42,7 @@ const userSchema = new Schema(
     groups: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Group'
+        ref: 'Group',
       }
     ]
   },
@@ -60,6 +61,11 @@ userSchema.pre('save', async function (next) {
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
+};
+
+// method to populate groups
+userSchema.methods.populateGroups = function() {
+  return this.populate('groups').execPopulate();
 };
 
 const User = model('User', userSchema);
