1c5a92d5579b9b2290c2543d7cdbf852d2258d7e
noodle77 <153270885+noodle77@users.noreply.github.com>
2024-06-06 19:36:03 -0500

    Added Group model including fields for name, subject, and description; a subdoc for members which references the User model, and a schedule subdoc along with a simple subdoc schema

--- Develop/server/models/Group.js
+++ Develop/server/models/Group.js
@@ -1,19 +1,62 @@
 const { Schema, model } = require('mongoose');
+const userSchema = require('./User.js');
 
+// Define Schedule subdoc schema
+const scheduleSchema = new Schema({
+    sunday: { 
+        type: Boolean, 
+        default: false 
+    },
+    monday: { 
+        type: Boolean, 
+        default: false 
+    },
+    tuesday: { 
+        type: Boolean, 
+        default: false 
+    },
+    wednesday: { 
+        type: Boolean, 
+        default: false 
+    },
+    thursday: { 
+        type: Boolean, 
+        default: false 
+    },
+    friday: { 
+        type: Boolean, 
+        default: false 
+    },
+    saturday: { 
+        type: Boolean, 
+        default: false 
+    },
+});
+
+// Define Group schema
 const groupSchema = new Schema({
     name: {
         type: String,
         required: true,
-        unique: true,
     },
-    members: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
-})
+    subject: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    members: {
+        type: [userSchema],
+        required: true,
+    },
+    schedule: {
+        type: [scheduleSchema],
+        required: false,
+    },
+});
 
 const Group = model('Group', groupSchema);
 
-module.exports = Group;
+module.exports = { groupSchema, Group };
