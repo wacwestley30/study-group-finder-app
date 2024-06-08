a7b5bf64fc0273a088b71a6dc2e2d04f9f73c90a
noodle77 <153270885+noodle77@users.noreply.github.com>
2024-06-07 20:17:48 -0500

    added updated user model with all optional fields covered

--- Develop/server/models/User.js
+++ Develop/server/models/User.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new Schema(
   {
-    
     firstName: {
       type: String,
       required: false,
@@ -61,11 +60,6 @@ userSchema.pre('save', async function (next) {
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
-};
-
-// method to populate groups
-userSchema.methods.populateGroups = function() {
-  return this.populate('groups');
 };
 
 const User = model('User', userSchema);
