b895e32845d278945fd0d2bed09f9e865af197ce
bcgosselin <bcgosselin@gmail.com>
2024-06-09 07:44:47 -0500

    Updated seed scripts to map usernames to ObjectIds based on model requirements

--- Develop/server/seeds/seed.js
+++ Develop/server/seeds/seed.js
@@ -1,27 +1,40 @@
-const db = require('../config/connection')
+const db = require('../config/connection');
 const { User, Group } = require('../models');
 const cleanDB = require('./cleanDB');
 const usersData = require('./userSeed');
-const groupsData = require('./groupSeed')
+const groupsData = require('./groupSeed');
+
+const getUsersObjectIds = async () => {
+  const users = await User.find();
+  const userMap = {};
+  users.forEach(user => {
+    userMap[user.username] = user._id;
+  });
+  return userMap;
+};
 
 db.once('open', async () => {
   try {
-
-    await cleanDB('Group', 'groups');  
-
+    await cleanDB('Group', 'groups');
     await cleanDB('User', 'users');
-
+    
     await User.create(usersData);
-
-    await Group.create(groupsData);
-
-    } catch (error) {
-        
-        console.error('Error inserting seed data:', error);
-        process.exit(1);
-        
-    }
-
+    
+    const userMap = await getUsersObjectIds();
+    
+    const updatedGroupsData = groupsData.map(group => {
+      return {
+        ...group,
+        members: group.members.map(username => userMap[username])
+      };
+    });
+    
+    await Group.create(updatedGroupsData);
+    
     console.log('Seed data inserted!');
     process.exit(0);
+  } catch (error) {
+    console.error('Error inserting seed data:', error);
+    process.exit(1);
+  }
 });
